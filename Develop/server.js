f41e0d35df45d37903c826e45ab1b9938a5ad5b2
dshaw1242 <david.shaw1242@gmail.com>
2021-05-20 21:44:14 -0600

    finished html routes, started work on notes api route

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// const {getAndRenderNotes} = require('./public/assets/js/index.js');
+const { db } = require('./db/db.json');
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,12 +14,35 @@ app.use(express.static('public'));
 
 
 
-// routes
+// Routes
+
+// HTML Routes
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname,'./public/notes.html'));
+});
+
+// app.get('/note/:note', (req,res) => {
+//     getAndRenderNotes();
+//     res.json(db);
+// })
+app.get('/api/notes', (req, res) => {
+    const note = db;
+    console.log(note);
+    res.json(note);
+});
+
+app.post('/api/notes', (req, res) => {
+    const note = req.body;
+    console.log(note);
+    res.json(note);
+
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
+
