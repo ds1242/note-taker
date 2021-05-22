9553b7d37897a0cfe72c7d6c84b865f6b83d05dd
dshaw1242 <david.shaw1242@gmail.com>
2021-05-22 10:16:33 -0600

    setup api/note route so page loads correctly now

--- Develop/server.js
+++ Develop/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // const {getAndRenderNotes} = require('./public/assets/js/index.js');
-const { db } = require('./db/db.json');
+const db  = require('./db/db.json');
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -25,19 +25,18 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname,'./public/notes.html'));
 });
 
-// app.get('/note/:note', (req,res) => {
-//     getAndRenderNotes();
-//     res.json(db);
-// })
+
 app.get('/api/notes', (req, res) => {
     const note = db;
-    console.log(note);
-    res.json(note);
+    console.log('api get note console log ' + note);
+    return res.json(note);
 });
+
+
 
 app.post('/api/notes', (req, res) => {
     const note = req.body;
-    console.log(note);
+    console.log('api push conosle log ' + note);
     res.json(note);
 
 })
